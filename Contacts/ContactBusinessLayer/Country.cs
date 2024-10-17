81ee3c202129c5a6ab01e2d49e4af4f3775a7cbf
ahmadamerr <ahmedashraf200595@gmail.com>
2024-10-17 05:49:33 +0300

    Contacts App Using C# Windows Form Application With 3 Tire Architecture

---
+++ Contacts/ContactBusinessLayer/Country.cs
@@ -0,0 +1,64 @@
+﻿namespace WFADVLD
+{
+    partial class frmManageApplicationsTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pbApplicationTypesmage = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblRecordsCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editApplicationTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbApplicationTypesmage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTitle.Location = new System.Drawing.Point(147, 184);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(446, 39);
+            this.lblTitle.TabIndex = 112;
+            this.lblTitle.Text = "Manage Application Types";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbApplicationTypesmage
+            // 
+            this.pbApplicationTypesmage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbApplicationTypesmage.Image = global::WFADVLD.Properties.Resources.Application_Types_512;
+            this.pbApplicationTypesmage.InitialImage = null;
+            this.pbApplicationTypesmage.Location = new System.Drawing.Point(274, 14);
+            this.pbApplicationTypesmage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbApplicationTypesmage.Name = "pbApplicationTypesmage";
+            this.pbApplicationTypesmage.Siz
---
+++ Contacts/ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContactsDataAccessLayer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=. ; Database=ContactsDB ; User Id=sa ; Password=sa123456 ;";
+       
+
+    }
+}
